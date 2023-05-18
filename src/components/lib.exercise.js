cb4dbd46bd27af491e5f2ec8c0d7b24b6467f8a0
jeanmarcjones <20986524+jeanmarcjones@users.noreply.github.com>
2023-05-18 16:42:58 +0100

    extra 1, 2 and 3

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -1,34 +1,35 @@
-import styled from '@emotion/styled'
+import styled from '@emotion/styled/macro'
+import { keyframes } from '@emotion/core'
 import { Dialog as ReachDialog } from '@reach/dialog'
+import { FaSpinner } from 'react-icons/fa'
+import * as colors from 'styles/colors'
+import * as mq from 'styles/media-queries'
 
 const buttonVariants = {
   primary: {
-    backgroundColor: '#3f51b5',
-    color: 'white',
+    background: colors.indigo,
+    color: colors.base,
   },
   secondary: {
-    backgroundColor: '#f1f2f7',
-    color: '#434449',
+    background: colors.gray,
+    color: colors.text,
   },
 }
-
-const Button = styled.button({
-  padding: '10px 15px',
-  border: '0',
-  lineHeight: '1',
-  borderRadius: '3px',
-}, props => buttonVariants[props.variant])
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+  },
+  props => buttonVariants[props.variant],
+)
 
 const Input = styled.input({
   borderRadius: '3px',
-  border: '1px solid #f1f1f4',
-  background: '#f1f2f7',
+  border: `1px solid ${colors.gray10}`,
+  background: colors.gray,
   padding: '8px 12px',
-})
-
-const FormGroup = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
 })
 
 const CircleButton = styled.button({
@@ -40,9 +41,9 @@ const CircleButton = styled.button({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  background: 'white',
-  color: '#434449',
-  border: `1px solid #f1f1f4`,
+  background: colors.base,
+  color: colors.text,
+  border: `1px solid ${colors.gray10}`,
   cursor: 'pointer',
 })
 
@@ -52,10 +53,31 @@ const Dialog = styled(ReachDialog)({
   paddingBottom: '3.5em',
   boxShadow: '0 10px 30px -5px rgba(0, 0, 0, 0.2)',
   margin: '20vh auto',
-  '@media (max-width: 991px)': {
+  [mq.small]: {
     width: '100%',
     margin: '10vh auto',
   },
 })
 
-export { CircleButton, Dialog, Button, Input, FormGroup }
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
+
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const Spinner = styled(FaSpinner)({
+  animation: `${rotate} 1s ease infinite`,
+})
+Spinner.defaultProps = {
+  'aria-label': 'loading'
+}
+
+export { Button, Input, CircleButton, Dialog, FormGroup, Spinner }
