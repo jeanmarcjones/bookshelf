14697506a57465bb2e59612b17c6ae32a103dd13
jeanmarcjones <20986524+jeanmarcjones@users.noreply.github.com>
2023-05-22 16:12:44 +0100

    extra 01

--- src/setupProxy.exercise.js
+++ src/setupProxy.exercise.js
@@ -1,4 +1,8 @@
-// 🐨 you don't need to do anything in this file for the exercise. This is
-// just here for the extra credit. See the instructions for more info.
+function proxy(app) {
+  // add the redirect handler here
+  app.get(/\/$/, function (req, res) {
+    res.redirect('discover')
+  })
+}
 
-module.exports = () => {}
+module.exports = proxy
