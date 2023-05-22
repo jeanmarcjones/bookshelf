71c54814b3d51e9da0f6a1638675583d83be5858
jeanmarcjones <20986524+jeanmarcjones@users.noreply.github.com>
2023-05-22 14:40:05 +0100

    extra 02

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -6,21 +6,32 @@ import * as auth from 'auth-provider'
 import { AuthenticatedApp } from './authenticated-app'
 import { UnauthenticatedApp } from './unauthenticated-app'
 import { client } from './utils/api-client.exercise'
+import { useAsync } from './utils/hooks'
+import { FullPageSpinner } from './components/lib'
+import { danger } from 'styles/colors'
 
 function App() {
-  const [user, setUser] = useState(null)
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync()
 
   useEffect(() => {
     const checkToken = async () => {
       const token = await auth.getToken()
       if (token) {
-        const { user } = await client('me', { token })
-        setUser(user)
+        await run(client('me', { token }))
       }
     }
 
     checkToken()
-  }, [])
+  }, [run, setUser])
 
   const login = form => auth.login(form).then(u => setUser(u))
   const register = form => auth.register(form).then(u => setUser(u))
@@ -29,11 +40,31 @@ function App() {
     setUser(null)
   }
 
-  return user ? (
-    <AuthenticatedApp user={user} logout={logout}/>
-  ) : (
-    <UnauthenticatedApp login={login} register={register}/>
+  if (isIdle || isLoading) return <FullPageSpinner />
+
+  if (isError) return (
+    <div
+      css={{
+        color: danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
   )
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout}/>
+    ) : (
+      <UnauthenticatedApp login={login} register={register}/>
+    )
+  }
 }
 
 export { App }
