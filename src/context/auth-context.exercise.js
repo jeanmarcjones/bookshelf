a233da3e0b760fefab9032a456991f852480182a
jeanmarcjones <20986524+jeanmarcjones@users.noreply.github.com>
2024-02-01 15:13:49 +0000

    extra credit 02

--- src/context/auth-context.exercise.js
+++ src/context/auth-context.exercise.js
@@ -1,15 +1,91 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
 
-const AuthContext = React.createContext()
+import * as React from 'react'
+import { useAsync } from '../utils/hooks'
+import * as auth from '../auth-provider'
+import { queryCache } from 'react-query'
+import { FullPageSpinner } from '../components/lib'
+import * as colors from '../styles/colors'
+import { client } from '../utils/api-client'
 
-const useAuth = () => {
-  const context = React.useContext(AuthContext)
+async function getUser() {
+  let user = null
 
-  if (context === undefined) {
-    throw new Error('useAuth must be used within a AuthContext.Provider');
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', { token })
+    user = data.user
   }
 
+  return user
+}
+
+const AuthContext = React.createContext()
+AuthContext.displayName = 'AuthContext'
+
+function useAuth() {
+  const context = React.useContext(AuthContext)
+  if (context === undefined) {
+    throw new Error(`useAuth must be used within a AuthProvider`)
+  }
   return context
 }
 
-export {AuthContext, useAuth}
+function AuthProvider({children}) {
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    isSuccess,
+    run,
+    setData,
+  } = useAsync()
+
+  React.useEffect(() => {
+    run(getUser())
+  }, [run])
+
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
+  const logout = () => {
+    auth.logout()
+    queryCache.clear()
+    setData(null)
+  }
+
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    const value = { user, login, register, logout }
+    return (
+      <AuthContext.Provider value={value}>
+        {children}
+      </AuthContext.Provider>
+    )
+  }
+}
+
+export {AuthProvider, useAuth}
