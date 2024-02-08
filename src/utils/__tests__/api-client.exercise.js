7bc0b5f457610abc8160fd1df86a35d7ddbe6011
jeanmarcjones <20986524+jeanmarcjones@users.noreply.github.com>
2024-02-08 16:58:23 +0000

    extra credit 01

--- src/utils/__tests__/api-client.exercise.js
+++ src/utils/__tests__/api-client.exercise.js
@@ -1,44 +1,39 @@
-// 🐨 you'll need the test server
-// 💰 the way that our tests are set up, you'll find this in `src/test/server/test-server.js`
 import {server, rest} from 'test/server'
-// 🐨 grab the client
 import {client} from '../api-client'
+import {queryCache} from 'react-query'
+import * as auth from 'auth-provider'
 
-// 🐨 add a beforeAll to start the server with `server.listen()`
+jest.mock('react-query')
+jest.mock('auth-provider')
+
+const apiURL = process.env.REACT_APP_API_URL
+
+// enable API mocking in test runs using the same request handlers
+// as for the client-side mocking.
 beforeAll(() => server.listen())
-// 🐨 add an afterAll to stop the server when `server.close()`
 afterAll(() => server.close())
-// 🐨 afterEach test, reset the server handlers to their original handlers
-// via `server.resetHandlers()`
 afterEach(() => server.resetHandlers())
 
-// 🐨 flesh these out:
-const apiURL = process.env.REACT_APP_API_URL
-const endpoint = 'test-endpoint'
-const mockResult = {mockValue: 'VALUE'}
-
-test('calls fetch at the endpoint with the arguments for GET requests', async () => {
-  // 🐨 add a server handler to handle a test request you'll be making
-  // 💰 because this is the first one, I'll give you the code for how to do that.
+test('makes GET requests to the given endpoint', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
   server.use(
     rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       return res(ctx.json(mockResult))
     }),
   )
 
-  // 🐨 call the client (don't forget that it's asynchronous)
-  const received = await client(endpoint)
-  // 🐨 assert that the resolved value from the client call is correct
-  expect(received).toEqual(mockResult)
+  const result = await client(endpoint)
+
+  expect(result).toEqual(mockResult)
 })
 
 test('adds auth token when a token is provided', async () => {
-  // 🐨 create a fake token (it can be set to any string you want)
-  const token = 'token'
-  // 🐨 create a "request" variable with let
+  const token = 'FAKE_TOKEN'
+
   let request
-  // 🐨 create a server handler to handle a test request you'll be making
-  // 🐨 inside the server handler, assign "request" to "req" so we can use that
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
   server.use(
     rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       request = req
@@ -46,46 +41,67 @@ test('adds auth token when a token is provided', async () => {
     }),
   )
 
-  // 🐨 call the client with the token (note that it's async)
   await client(endpoint, {token})
-  // 🐨 verify that `request.headers.get('Authorization')` is correct (it should include the token)
+
   expect(request.headers.get('Authorization')).toBe(`Bearer ${token}`)
 })
 
 test('allows for config overrides', async () => {
-  // 🐨 do a very similar setup to the previous test
   let request
+  const endpoint = 'test-endpoint'
+  const mockResult = {mockValue: 'VALUE'}
   server.use(
-    rest.post(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+    rest.put(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       request = req
       return res(ctx.json(mockResult))
     }),
   )
-  // 🐨 create a custom config that specifies properties like "mode" of "cors" and a custom header
-  const config = {
-    headers: {
-      custom: 'header',
-    },
-    method: 'POST',
+
+  const customConfig = {
+    method: 'PUT',
+    headers: {'Content-Type': 'fake-type'},
   }
-  // 🐨 call the client with the endpoint and the custom config
-  await client(endpoint, config)
-  // 🐨 verify the request had the correct properties
-  expect(request.headers.get('custom')).toBe('header')
+
+  await client(endpoint, customConfig)
+
+  expect(request.headers.get('Content-Type')).toBe(
+    customConfig.headers['Content-Type'],
+  )
 })
 
 test('when data is provided, it is stringified and the method defaults to POST', async () => {
-  // 🐨 create a mock data object
-  // 🐨 create a server handler very similar to the previous ones to handle the post request
-  //    💰 Use rest.post instead of rest.get like we've been doing so far
+  const endpoint = 'test-endpoint'
   server.use(
     rest.post(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
       return res(ctx.json(req.body))
     }),
   )
-  // 🐨 call client with an endpoint and an object with the data
-  //    💰 client(endpoint, {data})
-  const received = await client(endpoint, {data: mockResult})
-  // 🐨 verify the request.body is equal to the mock data object you passed
-  expect(received).toEqual(mockResult)
+  const data = {a: 'b'}
+  const result = await client(endpoint, {data})
+
+  expect(result).toEqual(data)
 })
+
+test('should handle unsuccessful status codes', async () => {
+  const endpoint = 'test-endpoint'
+  server.use(
+      rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+        return res(ctx.status(400), ctx.json({message: '400 Bad Request'}))
+      }),
+  )
+
+  await expect(client(endpoint)).rejects.toEqual({message: '400 Bad Request'})
+})
+
+test('should handle a unauthorised request', async () => {
+  const endpoint = 'test-endpoint'
+  server.use(
+      rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+        return res(ctx.status(401), ctx.json({message: '401 Unauthorized'}))
+      }),
+  )
+
+  await expect(client(endpoint)).rejects.toEqual({message: 'Please re-authenticate.'})
+  expect(queryCache.clear).toHaveBeenCalledTimes(1)
+  expect(auth.logout).toHaveBeenCalledTimes(1)
+});
--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,6 +1,5 @@
-import {formatDate} from "../misc";
+import {formatDate} from '../misc'
 
 test('formatDate formats the date to look nice', () => {
-    const received = formatDate(new Date('2011-10-10T14:48:00.000Z'))
-    expect(received).toBe('Oct 11')
+  expect(formatDate(new Date('October 18, 1988'))).toBe('Oct 88')
 })
