890ac00fd111087a2ab0cedd5408834a9867c7da
jeanmarcjones <20986524+jeanmarcjones@users.noreply.github.com>
2024-02-08 08:57:39 +0000

    exercise

--- src/unauthenticated-app.exercise.js
+++ src/unauthenticated-app.exercise.js
@@ -12,12 +12,12 @@ import {
   ErrorMessage,
   // 💣 when you're all done, you won't need this Dialog anymore
   // you can remove this now or later when you've finished
-  Dialog,
 } from './components/lib'
 // 🐨 import all the Modal compound components you created in ./components/modal
 import {Logo} from './components/logo'
 import {useAuth} from './context/auth-context'
 import {useAsync} from './utils/hooks'
+import { Modal, ModalContents, ModalDismissButton, ModalOpenButton } from './components/modal.exercise'
 
 function LoginForm({onSubmit, submitButton}) {
   const {isLoading, isError, error, run} = useAsync()
@@ -71,36 +71,6 @@ function LoginForm({onSubmit, submitButton}) {
 }
 
 // 💣 when you're all done, you'll be able to completely delete this
-function LoginFormModal({
-  onSubmit,
-  modalTitleText,
-  modalLabelText,
-  submitButton,
-  openButton,
-}) {
-  const [isOpen, setIsOpen] = React.useState(false)
-
-  return (
-    <React.Fragment>
-      {React.cloneElement(openButton, {onClick: () => setIsOpen(true)})}
-      <Dialog
-        aria-label={modalLabelText}
-        isOpen={isOpen}
-        onDismiss={() => setIsOpen(false)}
-      >
-        <div css={{display: 'flex', justifyContent: 'flex-end'}}>
-          {/* 💰 here's what you should put in your <ModalDismissButton> */}
-          <CircleButton onClick={() => setIsOpen(false)}>
-            <VisuallyHidden>Close</VisuallyHidden>
-            <span aria-hidden>×</span>
-          </CircleButton>
-        </div>
-        <h3 css={{textAlign: 'center', fontSize: '2em'}}>{modalTitleText}</h3>
-        <LoginForm onSubmit={onSubmit} submitButton={submitButton} />
-      </Dialog>
-    </React.Fragment>
-  )
-}
 
 function UnauthenticatedApp() {
   const {login, register} = useAuth()
@@ -130,20 +100,47 @@ function UnauthenticatedApp() {
              it did when you started, but the extra credits will help clean
              things up a bit.
         */}
-        <LoginFormModal
-          onSubmit={login}
-          modalTitleText="Login"
-          modalLabelText="Login form"
-          submitButton={<Button variant="primary">Login</Button>}
-          openButton={<Button variant="primary">Login</Button>}
-        />
-        <LoginFormModal
-          onSubmit={register}
-          modalTitleText="Register"
-          modalLabelText="Registration form"
-          submitButton={<Button variant="secondary">Register</Button>}
-          openButton={<Button variant="secondary">Register</Button>}
-        />
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="primary">Login</Button>
+          </ModalOpenButton>
+
+          <ModalContents aria-label="Login form">
+            <ModalDismissButton>
+              <CircleButton>
+                <VisuallyHidden>Close</VisuallyHidden>
+                <span aria-hidden>×</span>
+              </CircleButton>
+            </ModalDismissButton>
+
+            <h3 css={{textAlign: 'center', fontSize: '2em'}}>
+              Login
+            </h3>
+
+            <LoginForm onSubmit={login} submitButton={<Button variant="primary">Login</Button>} />
+          </ModalContents>
+        </Modal>
+
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="secondary">Register</Button>
+          </ModalOpenButton>
+
+          <ModalContents aria-label="Registration form">
+            <ModalDismissButton>
+              <CircleButton>
+                <VisuallyHidden>Close</VisuallyHidden>
+                <span aria-hidden>×</span>
+              </CircleButton>
+            </ModalDismissButton>
+
+            <h3 css={{textAlign: 'center', fontSize: '2em'}}>
+              Login
+            </h3>
+
+            <LoginForm onSubmit={register} submitButton={<Button variant="secondary">Register</Button>} />
+          </ModalContents>
+        </Modal>
       </div>
     </div>
   )
